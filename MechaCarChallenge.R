61fcb8682b15091b8abd76b04a197301647c138d
sib <sibtain.janmohamed@gmail.com>
2021-05-28 12:53:58 -0400

    Challenge files

---
+++ MechaCarChallenge.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(dplyr)
+
+car_data <- read.csv("MechaCar_mpg.csv",stringsAsFactors = FALSE,check.names = FALSE)
+
+car_LG <- lm(mpg~vehicle_length+vehicle_weight+ground_clearance+spoiler_angle+AWD,data=car_data)
+
+summary(car_LG)
+
+suspension_data <- read.csv("Suspension_Coil.csv",stringsAsFactors = FALSE,check.names = FALSE)
+
+all_summary <- suspension_data %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),Standard=sd(PSI))
+
+lots_summary <- suspension_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),Standard=sd(PSI))
